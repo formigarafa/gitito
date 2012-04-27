b872c04b4e1b432c2d10b1ad67c7dcee905507a7
Rafael Marcelino dos Santos <formigarafa@gmail.com>
2012-04-27 23:32:50 +1200

    scaping char on regex

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,7 +1,7 @@
 class Repository < ActiveRecord::Base
 	attr_readonly :name
 
-	validates :name, :presence => true, :format => { :with => /^[a-zA-Z0-9-_.]*$/ }
+	validates :name, :presence => true, :format => { :with => /^[a-zA-Z0-9\-_.]*$/ }
 
 	def self.repos_root
 		"#{Rails.root}/db/users_repositories"
