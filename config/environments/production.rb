28e37cacaf0be3b425e399607436762f7fa4cf12
Rafael Marcelino dos Santos <formigarafa@gmail.com>
2012-04-30 00:33:44 +1200

    action mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Gitito::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'xaa.xlii.com.br' }
 end
