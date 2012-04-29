4737af67500d04d043e633ba2099c68908955f4d
Rafael Marcelino dos Santos <formigarafa@gmail.com>
2012-04-29 17:43:16 +1200

    devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Gitito::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "some#where"
 
   # See how all your routes lay out with "rake routes"
 
