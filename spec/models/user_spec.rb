afbf267b74c720189960f2c27b0e84dae20f60db
Rafael Marcelino dos Santos <formigarafa@gmail.com>
2012-04-29 18:08:19 +1200

    fix variable name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,48 +11,48 @@ describe User do
 		end
 
 		it "validates with matching password_confirmation" do
-			repo = valid_user
-			repo.password = 'p4ssw0rd'
-			repo.password_confirmation = 'p4ssw0rd'
-			repo.should be_valid
+			user = valid_user
+			user.password = 'p4ssw0rd'
+			user.password_confirmation = 'p4ssw0rd'
+			user.should be_valid
 		end
 
 
 		it "fails with no username" do
-			repo = valid_user
-			repo.username = nil
-			repo.should_not be_valid
+			user = valid_user
+			user.username = nil
+			user.should_not be_valid
 		end
 
 		it "fails with no email" do
-			repo = valid_user
-			repo.email = nil
-			repo.should_not be_valid
+			user = valid_user
+			user.email = nil
+			user.should_not be_valid
 		end
 
 		it "fails with no password" do
-			repo = valid_user
-			repo.password = nil
-			repo.should_not be_valid
+			user = valid_user
+			user.password = nil
+			user.should_not be_valid
 		end
 
 		it "fails when password does not match password_confirmation" do
-			repo = valid_user
-			repo.password = '1qaz2wsx'
-			repo.password_confirmation = '0okm9ijn'
-			repo.should_not be_valid
+			user = valid_user
+			user.password = '1qaz2wsx'
+			user.password_confirmation = '0okm9ijn'
+			user.should_not be_valid
 		end
 
 		it "fails with complicated username (special characters)" do
-			repo = valid_user
-			repo.username = "f*cking_user"
-			repo.should_not be_valid
+			user = valid_user
+			user.username = "f*cking_user"
+			user.should_not be_valid
 		end
 
 		it "fails with spaces on username" do
-			repo = valid_user
-			repo.username = "some user name"
-			repo.should_not be_valid
+			user = valid_user
+			user.username = "some user name"
+			user.should_not be_valid
 		end
 
 	end
