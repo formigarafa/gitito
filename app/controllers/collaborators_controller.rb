f61c7bdab8f520e601b695083153dfac9a1d4876
Rafael Marcelino dos Santos <formigarafa@gmail.com>
2012-06-07 23:19:08 +1200

    added collaborator controller
    collaborator controller: take care of creating and removing
    collaborators from repositories

---
+++ app/controllers/collaborators_controller.rb
@@ -0,0 +1,18 @@
+class CollaboratorsController < InheritedResources::Base
+  actions :create, :destroy
+  belongs_to :repository
+
+  def create
+    user = User.where(:username => params[:collaborator][:username]).find(:first)
+    @collaborator = Repository.find(params[:repository_id]).collaborators.new :user => user
+    create! do |success, failure|
+      success.html do
+        redirect_to repository_url(@collaborator.repository)
+      end
+      failure.html do
+        flash[:error] = @collaborator.errors.values.join(", ")
+        redirect_to repository_url(@collaborator.repository)
+      end
+    end
+  end
+end
