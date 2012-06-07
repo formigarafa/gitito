bb5c14feca001539c39a5829c521c82b2214ae95
Rafael Marcelino dos Santos <formigarafa@gmail.com>
2012-06-07 20:33:45 +1200

    indentation converted to spaces

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,54 +1,54 @@
 class Repository < ActiveRecord::Base
-	belongs_to :user
+  belongs_to :user
 
-	attr_readonly :name
+  attr_readonly :name
 
-	validates :name, :presence => true, :format => { :with => /^[a-zA-Z0-9\-_.]*$/ }, :uniqueness => {:scope => :user_id}
-	validates :user, :presence => true
+  validates :name, :presence => true, :format => { :with => /^[a-zA-Z0-9\-_.]*$/ }, :uniqueness => {:scope => :user_id}
+  validates :user, :presence => true
 
-	def self.repos_root
-		"#{Rails.root}/db/users_repositories/#{Rails.env}"
-	end
+  def self.repos_root
+    "#{Rails.root}/db/users_repositories/#{Rails.env}"
+  end
 
-	def server_path
-		"#{self.class.repos_root}/#{user.username}/#{name}.git"
-	end
+  def server_path
+    "#{self.class.repos_root}/#{user.username}/#{name}.git"
+  end
 
-	def ssh_user
-		Rails.configuration.gitito[:ssh_user]
-	end
+  def ssh_user
+    Rails.configuration.gitito[:ssh_user]
+  end
 
-	def ssh_host
-		Rails.configuration.gitito[:ssh_host]
-	end
+  def ssh_host
+    Rails.configuration.gitito[:ssh_host]
+  end
 
-	def url
-		"#{ssh_user}@#{ssh_host}:#{user.username}/#{name}.git"
-	end
+  def url
+    "#{ssh_user}@#{ssh_host}:#{user.username}/#{name}.git"
+  end
 
-	def folder_exists?
-		File.exists? server_path
-	end
+  def folder_exists?
+    File.exists? server_path
+  end
 
-	def structure_ok?
-		Rugged::Repository.new server_path
-	rescue
-		false
-	end
+  def structure_ok?
+    Rugged::Repository.new server_path
+  rescue
+    false
+  end
 
-	def create_structure
-    	Rugged::Repository.init_at(server_path, bare=true) unless folder_exists?
-  	end
+  def create_structure
+    Rugged::Repository.init_at(server_path, bare=true) unless folder_exists?
+  end
 
-  	def remove_structure
-    	FileUtils.remove_dir(server_path) if File.directory? server_path
-  	end
+  def remove_structure
+    FileUtils.remove_dir(server_path) if File.directory? server_path
+  end
 
-	after_create do
-		create_structure
-	end
+  after_create do
+    create_structure
+  end
 
-	after_destroy do
-		remove_structure
-	end
+  after_destroy do
+    remove_structure
+  end
 end
