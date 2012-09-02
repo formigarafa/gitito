162e21fd5070598619dba4dfb6760af2d54e226b
Rafael Marcelino dos Santos <formigarafa@gmail.com>
2012-09-02 23:58:20 +1200

    starting simplecov before rails.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,17 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
+if ENV['coverage'] == 'on'
+  require 'simplecov'  
+  SimpleCov.start 'rails' do
+    add_filter "vendor/ruby/"
+  end
+end
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
-if ENV['coverage'] == 'on'
-  require 'simplecov'  
-  SimpleCov.start 'rails'
-end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
