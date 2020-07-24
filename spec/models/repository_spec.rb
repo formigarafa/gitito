29236cea8a5a6bd5bceb3152de51ae041bb482f6
Rafael Santos <formigarafa@gmail.com>
2020-07-24 16:13:02 +1200

    update rspec syntax

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -9,67 +9,67 @@ describe Repository do
 
   context "validity" do
     it "validates with valid attributes" do
-      valid_repo.should be_valid
+      expect(valid_repo).to be_valid
     end
 
     it "fails with no name" do
       repo = valid_repo
       repo.name = nil
-      repo.should_not be_valid
+      expect(repo).not_to be_valid
     end
 
     it "fails with no owner" do
       repo = valid_repo
       repo.user = nil
-      repo.should_not be_valid
+      expect(repo).not_to be_valid
     end
 
     it "fails with complicated name (special characters)" do
       repo = valid_repo
       repo.name = "f*cking_repo"
-      repo.should_not be_valid
+      expect(repo).not_to be_valid
     end
 
     it "fails with spaces on name" do
       repo = valid_repo
       repo.name = "some repo"
-      repo.should_not be_valid
+      expect(repo).not_to be_valid
     end
 
     it "is not allowed to one user have two repositories with the same name" do
       user = mock_model(User, username: "juca")
       repo1 = described_class.new
-      repo1.stub(:create_structure)
+      allow(repo1).to receive(:create_structure)
       repo1.name = "same_name"
       repo1.user = user
       repo1.save
 
       repo2 = described_class.new
-      repo2.stub(:create_structure)
+      allow(repo2).to receive(:create_structure)
       repo2.user = user
       repo2.name = "same_name"
 
-      repo2.should_not be_valid
+      expect(repo2).not_to be_valid
     end
 
     it "is allowed for two different user have two repositories with the same name" do
       repo1 = described_class.new
       repo1.name = "same_name"
-      repo1.stub(:create_structure)
+      allow(repo1).to receive(:create_structure)
       repo1.user = mock_model(User, username: "juca")
       repo1.save
 
       repo2 = described_class.new
-      repo2.stub(:create_structure)
+      allow(repo2).to receive(:create_structure)
       repo2.user = mock_model(User, username: "drica.silva")
       repo2.name = "same_name"
-      repo2.should be_valid
+      expect(repo2).to be_valid
     end
   end
 
   it "forbides repository name changes" do
     repo = valid_repo
-    repo.stub(:create_structure)
+    allow(repo).to receive(:create_structure)
     repo.save
     first_used_name = repo.name
 
@@ -78,66 +78,66 @@ describe Repository do
 
     repo.reload
 
-    repo.name.should == first_used_name
+    expect(repo.name).to eq first_used_name
   end
 
   context "path" do
     it "has method to get where I can find all repositories" do
-      described_class.repos_root.should == "#{Rails.root}/db/users_repositories/#{Rails.env}"
+      expect(described_class.repos_root).to eq "#{Rails.root}/db/users_repositories/#{Rails.env}"
     end
 
     it "has method to get repository path on file server" do
       repo = described_class.new name: "tilt", user: stub_model(User, username: "formigarafa")
-      described_class.stub(:repos_root).and_return("/rOOt")
-      repo.server_path.should == "/rOOt/formigarafa/tilt.git"
+      allow(described_class).to receive(:repos_root).and_return("/rOOt")
+      expect(repo.server_path).to eq "/rOOt/formigarafa/tilt.git"
     end
 
     it "has method to get repository access url" do
       repo = described_class.new name: "tilt", user: stub_model(User, username: "formigarafa")
-      repo.stub(:ssh_user).and_return("git")
-      repo.stub(:ssh_host).and_return("gitito.com")
+      allow(repo).to receive(:ssh_user).and_return("git")
+      allow(repo).to receive(:ssh_host).and_return("gitito.com")
 
-      repo.url.should == "git@gitito.com:formigarafa/tilt.git"
+      expect(repo.url).to eq "git@gitito.com:formigarafa/tilt.git"
     end
   end
 
   context "file tree" do
     it "can check if there is a folder on repository place" do
       repo = described_class.new
-      repo.should_receive(:server_path).and_return("#{Rails.root}/spec/fixtures/not_repo_test")
+      allow(repo).to receive(:server_path).and_return("#{Rails.root}/spec/fixtures/not_repo_test")
 
-      repo.folder_exists?.should be_truthy
+      expect(repo.folder_exists?).to be_truthy
     end
 
     it "can check if there is NOT a folder on repository place" do
       repo = described_class.new
-      repo.should_receive(:server_path).and_return("#{Rails.root}/spec/fixtures/somewhere_else")
+      allow(repo).to receive(:server_path).and_return("#{Rails.root}/spec/fixtures/somewhere_else")
 
-      repo.folder_exists?.should be_falsey
+      expect(repo.folder_exists?).to be_falsey
     end
 
     it "can check if there is a repo placed" do
       repo = described_class.new
-      repo.should_receive(:server_path).and_return("#{Rails.root}/spec/fixtures/repo_test.git")
+      expect(repo).to receive(:server_path).and_return("#{Rails.root}/spec/fixtures/repo_test.git")
 
-      repo.structure_ok?.should be_truthy
+      expect(repo.structure_ok?).to be_truthy
     end
 
     it "can check if there is NOT a repo placed" do
       repo = described_class.new
-      repo.should_receive(:server_path).and_return("#{Rails.root}/spec/fixtures/none_repo_test.git")
+      expect(repo).to receive(:server_path).and_return("#{Rails.root}/spec/fixtures/none_repo_test.git")
 
-      repo.structure_ok?.should be_falsey
+      expect(repo.structure_ok?).to be_falsey
     end
 
     it "create repository tree when doesn't exist one" do
       Dir.mktmpdir do |tmp_dir|
         repo = described_class.new
-        repo.stub(:server_path).and_return(tmp_dir + "/juca/repo_tmp")
+        allow(repo).to receive(:server_path).and_return(tmp_dir + "/juca/repo_tmp")
 
-        repo.folder_exists?.should be_falsey
+        expect(repo.folder_exists?).to be_falsey
         repo.create_structure
-        repo.folder_exists?.should be_truthy
+        expect(repo.folder_exists?).to be_truthy
       end
     end
 
@@ -145,10 +145,11 @@ describe Repository do
       Dir.mktmpdir do |_tmp_dir|
         repo = described_class.new
 
-        repo.stub(:folder_exists?).and_return(true)
-        Rugged::Repository.should_not_receive :init_at
+        allow(repo).to receive(:folder_exists?).and_return(true)
+        allow(Rugged::Repository).to receive :init_at
 
         repo.create_structure
+        expect(Rugged::Repository).not_to have_received :init_at
       end
     end
 
@@ -159,12 +160,12 @@ describe Repository do
         Dir.mkdir "#{tmp_dir}/1/2/3"
 
         repo = described_class.new
-        repo.stub(:server_path).and_return("#{tmp_dir}/1")
+        allow(repo).to receive(:server_path).and_return("#{tmp_dir}/1")
 
-        repo.folder_exists?.should be_truthy
+        expect(repo.folder_exists?).to be_truthy
         repo.remove_structure
 
-        repo.folder_exists?.should be_falsey
+        expect(repo.folder_exists?).to be_falsey
       end
     end
   end
@@ -176,11 +177,12 @@ describe Repository do
         subject.collaborators << collaborator
       end
 
-      subject.collaborators.should =~ collaborators
+      # =~
+      expect(subject.collaborators).to contain_exactly(*collaborators)
     end
 
     it "has no collaborators" do
-      subject.collaborators.size.should eq(0)
+      expect(subject.collaborators.size).to eq(0)
     end
   end
 
@@ -188,14 +190,14 @@ describe Repository do
     it "returns true for users equals to given parameter" do
       user = mock_model User
       subject.user = user
-      subject.owned_by?(user).should be_truthy
+      expect(subject.owned_by?(user)).to be_truthy
     end
 
     it "returns false for users equals to given parameter" do
       user = mock_model User
       different_user = mock_model User
       subject.user =   user
-      subject.owned_by?(different_user).should be_falsey
+      expect(subject.owned_by?(different_user)).to be_falsey
     end
   end
 
