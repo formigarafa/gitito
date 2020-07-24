50b889df0713f71178e72410033a206a1e4a676c
Rafael Santos <formigarafa@gmail.com>
2020-07-24 16:38:29 +1200

    fix some issues raised by rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -8,25 +9,29 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120607000942) do
+ActiveRecord::Schema.define(version: 20200724043231) do
 
-  create_table "collaborators", :force => true do |t|
+  create_table "collaborators", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "repository_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "repositories", :force => true do |t|
+  add_index "collaborators", ["repository_id", "user_id"], name: "index_collaborators_on_repository_id_and_user_id", unique: true
+
+  create_table "repositories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
   end
 
-  create_table "user_credentials", :force => true do |t|
+  add_index "repositories", ["name", "user_id"], name: "index_repositories_on_name_and_user_id", unique: true
+
+  create_table "user_credentials", force: :cascade do |t|
     t.string   "title"
     t.text     "key"
     t.integer  "user_id"
@@ -34,23 +39,25 @@ ActiveRecord::Schema.define(:version => 20120607000942) do
     t.datetime "updated_at"
   end
 
-  create_table "users", :force => true do |t|
+  add_index "user_credentials", ["key"], name: "index_user_credentials_on_key", unique: true
+
+  create_table "users", force: :cascade do |t|
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                              default: "", null: false
+    t.string   "encrypted_password",     limit: 128, default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",                      default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
